09c848a011d3ea663a05c6af95b27c58b080380b
mjturi <Drummer2001$>
2020-11-07 14:45:07 -0800

    first

--- faces.py
+++ faces.py
@@ -11,6 +11,8 @@ while(True):
     ret, frame = cap.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+    for (x,y,w,h) in faces:
+        print(x,y,w,h)
     cv2.imshow('frame', frame)
     if cv2.watKey(20) & 0xFF == ord('q'):
         break
