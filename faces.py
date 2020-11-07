d69fedc7afeb6ae7d6ec9407798122f851cf69eb
mjturi <Drummer2001$>
2020-11-07 14:51:15 -0800

    second

--- faces.py
+++ faces.py
@@ -3,18 +3,20 @@ import cv2
 
 face_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_alt.xml')
 
-
-
 cap = cv2.VideoCapture(0)
 
-while(True):
+while True:
     ret, frame = cap.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
-    for (x,y,w,h) in faces:
-        print(x,y,w,h)
+
+    for (x, y, w, h) in faces:
+        print(x, y, w, h)
+        roi_gray = gray[y:y+h, x:x+w]
+        img_item = 'my_img.png'
+        cv2.imwrite(img_item, roi_gray)
     cv2.imshow('frame', frame)
-    if cv2.watKey(20) & 0xFF == ord('q'):
+    if cv2.waitKey(20) & 0xFF == ord('q'):
         break
 
 cap.release()
